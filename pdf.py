7748499931731ec8b65e84c949da5e921b1d0bbd
Aditya Chawla <chawlaaditya8@gmail.com>
2016-04-14 23:20:08 +0530

    reading from csv

---
+++ pdf.py
@@ -0,0 +1,5 @@
+import pandas
+url = "https://goo.gl/vhm1eU"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names = names)
+print(data.shape)
