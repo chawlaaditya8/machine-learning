604cbc3b19f22f0daf62d38b0abe316b4a4450f3
Aditya Chawla <chawlaaditya8@gmail.com>
2016-05-19 18:57:18 +0530

    cleared directory

--- pdf.py
+++
@@ -1,5 +0,0 @@
-import pandas
-url = "https://goo.gl/vhm1eU"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-data = pandas.read_csv(url, names = names)
-print(data.shape)
